07bb3484afc24f6cdaf4c1bc0568be20d76f9a6f
Roberto Graham <14958777+RobertoGraham@users.noreply.github.com>
2026-03-16 10:02:29 +0000

    :wrench: Add wiring to application module.

--- payment-gateway-application/build.gradle.kts
+++ payment-gateway-application/build.gradle.kts
@@ -5,9 +5,11 @@ plugins {
 }
 
 dependencies {
+  implementation(project(":payment-gateway-domain"))
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
   implementation("org.springframework.boot:spring-boot-starter-restclient")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
-
+  runtimeOnly(project(":payment-gateway-adapter-acquiring-bank"))
+  runtimeOnly(project(":payment-gateway-adapter-payment-repository"))
   testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 }
--- payment-gateway-application/src/main/java/com/checkout/payment/gateway/PaymentGatewayApplication.java
+++ payment-gateway-application/src/main/java/com/checkout/payment/gateway/PaymentGatewayApplication.java
@@ -2,12 +2,21 @@ package com.checkout.payment.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import paymentgateway.domain.port.in.ProcessPaymentUseCase;
+import paymentgateway.domain.port.out.AcquiringBankPort;
+import paymentgateway.domain.port.out.PaymentRepositoryPort;
 
-@SpringBootApplication
-class PaymentGatewayApplication {
+@SpringBootApplication(proxyBeanMethods = false)
+final class PaymentGatewayApplication {
+
+  @Bean
+  static ProcessPaymentUseCase processPaymentUseCase(final AcquiringBankPort acquiringBankPort,
+      final PaymentRepositoryPort paymentRepositoryPort) {
+    return ProcessPaymentUseCase.newProcessPaymentUseCase(acquiringBankPort, paymentRepositoryPort);
+  }
 
   static void main() {
     SpringApplication.run(PaymentGatewayApplication.class);
   }
-
 }
