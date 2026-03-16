66e83f0982318b0e0809cd2e974ab50e76d9a816
Roberto Graham <14958777+RobertoGraham@users.noreply.github.com>
2026-03-16 16:10:27 +0000

    :fire: Remove Checkout.com files.

--- payment-gateway-application/build.gradle.kts
+++ payment-gateway-application/build.gradle.kts
@@ -1,16 +1,13 @@
 plugins {
   `spring-boot-conventions`
-  application
   id("org.springframework.boot")
 }
 
 dependencies {
   implementation(project(":payment-gateway-domain"))
-  implementation("org.springframework.boot:spring-boot-starter-webmvc")
-  implementation("org.springframework.boot:spring-boot-starter-restclient")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+  implementation("org.springframework.boot:spring-boot-autoconfigure")
   runtimeOnly(project(":payment-gateway-adapter-acquiring-bank"))
   runtimeOnly(project(":payment-gateway-adapter-payment-repository"))
   runtimeOnly(project(":payment-gateway-adapter-web"))
-  testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- payment-gateway-application/src/main/java/paymentgateway/application/PaymentGatewayApplication.java
+++ payment-gateway-application/src/main/java/paymentgateway/application/PaymentGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.checkout.payment.gateway;
+package paymentgateway.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
