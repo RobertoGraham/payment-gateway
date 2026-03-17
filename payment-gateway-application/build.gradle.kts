7412653993374554f19e1937b6871f27d9570479
Roberto Graham <14958777+RobertoGraham@users.noreply.github.com>
2026-03-17 22:18:13 +0000

    :monocle_face: Distributed tracing.

--- payment-gateway-application/build.gradle.kts
+++ payment-gateway-application/build.gradle.kts
@@ -9,5 +9,7 @@ dependencies {
   runtimeOnly(project(":payment-gateway-adapter-acquiring-bank"))
   runtimeOnly(project(":payment-gateway-adapter-payment-repository"))
   runtimeOnly(project(":payment-gateway-adapter-web"))
+  runtimeOnly("org.springframework.boot:spring-boot-micrometer-tracing-opentelemetry")
+  runtimeOnly("io.micrometer:micrometer-tracing-bridge-otel")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
